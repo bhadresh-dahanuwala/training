68f86a670b27a326c4f2221052d83606a01e33b6
bhadresh-dahanuwala <bhadresh.dahanuwala@gmail.com>
2023-06-14 22:22:24 -0400

    added simple dict

--- python/prac_dict.py
+++ python/prac_dict.py
@@ -1,1 +1,8 @@
-student = {101: {}}
+person = {
+    "first_name": "Aarush",
+    "last_name": "Dahanuwala",
+    "dob": "2010-08-30",
+    "weight_lbs": 95,
+    "height_inches": 59,
+    "friends": ["Shrirang", "Sid"],
+}
