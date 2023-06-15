0bacad618568bf54319ed64a9612e0754e841947
AarushDahanuwala <aarushyt10@gmail.com>
2023-06-14 21:04:44 -0400

    created file path variables

--- python/read_text_file.py
+++ python/read_text_file.py
@@ -1,8 +1,8 @@
 '''
 This program teaches how to read a text file.
 '''
-
-city_file = open('/Users/aarush/Documents/git/training/python/sample.txt', 'r')
+sample_file_path = '/Users/aarush/Documents/git/training/python/sample.txt'
+city_file = open(sample_file_path, 'r')
 for line in city_file:
     print(line.strip())
 
--- python/write_txt_file.py
+++ python/write_txt_file.py
@@ -1,8 +1,8 @@
 '''
 In this program we will learn how to write in a text file.
 '''
-
-flower_file = open("flowers.txt", "a")
+file_path = "flowers.txt"
+flower_file = open(file_path, "a")
 flower_file.write("lotus\n")
 
 flower_file.close()
